448f292550436c2b82f9d412b8a048cc30488c2a
joeulam <joeulam345@gmail.com>
2025-05-30 15:20:03 -0400

    Unsquish the login and sign up card

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,7 +8,7 @@ export default function Login() {
   
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-      <div className="w-1/4 max-w-sm md:max-w-3xl">
+      <div className="w-[100%] max-w-sm md:max-w-3xl sm:w-11/12">
         <LoginForm />
       </div>
     </div>
--- app/signUp/page.tsx
+++ app/signUp/page.tsx
@@ -8,8 +8,8 @@ export default function SignUp() {
   
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-      <div className="w-1/4 max-w-sm md:max-w-3xl">
-        <SignupForm />
+      <div className="w-[100%] max-w-sm md:max-w-3xl sm:w-11/12">
+      <SignupForm />
       </div>
     </div>
   );
